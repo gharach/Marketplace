dd35e06cbd4d83664f172048a83675966e545585
ali <cherloo>
2022-12-31 02:51:25 +0330

    Implemented Create seller method

--- app/Http/Controllers/Auth/UserAuthController.php
+++ app/Http/Controllers/Auth/UserAuthController.php
@@ -47,6 +47,25 @@ class UserAuthController extends Controller
         return response(['user' => auth()->user(), 'token' => $token, 'message' => 'Login Successful']);
 
     }
-    
+    public function createSeller(Request $request)
+    {
+
+        if (Gate::allows('isAdmin')) {
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|max:255',
+                'email' => 'required|email|unique:users',
+                'password' => 'required|confirmed'
+            ]);
+            $data = $validator->getData();
+            $data['password'] = bcrypt($request->password);
+            $data['role'] = 'seller';
+
+            $user = User::create($data);
+
+            $token = $user->createToken('API Token')->accessToken;
+
+            return response(['user' => $user, 'token' => $token, 'message' => 'Registration Successful']);
+        }
+    }
 
 }
