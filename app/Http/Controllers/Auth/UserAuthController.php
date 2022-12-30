65d515e7b8f2b9fdf23b65667117bd67eb01c601
ali <cherloo>
2022-12-31 02:50:08 +0330

    Create UserComtroller, Implimented Login and Register Method

---
+++ app/Http/Controllers/Auth/UserAuthController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Validator;
+
+class UserAuthController extends Controller
+{
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed'
+        ]);
+        $data = $validator->getData();
+        $data['password'] = bcrypt($request->password);
+        $data['role'] = 'customer';
+
+        $user = User::create($data);
+
+        $token = $user->createToken('API Token')->accessToken;
+
+        return response(['user' => $user, 'token' => $token, 'message' => 'Registration Successful']);
+
+    }
+
+    public function login(Request $request)
+    {
+        $data = $request->validate([
+            'email' => 'email|required',
+            'password' => 'required'
+        ]);
+
+        if (!auth()->attempt($data)) {
+            return response(['error_message' => 'Incorrect Details.
+            Please try again']);
+        }
+
+        $token = auth()->user()->createToken('API Token')->accessToken;
+
+        return response(['user' => auth()->user(), 'token' => $token, 'message' => 'Login Successful']);
+
+    }
+    
+
+}
